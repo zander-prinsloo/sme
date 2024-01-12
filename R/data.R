1d5562651c839c3d5e3d2c95f4cff5b15f85c085
zander-prinsloo <prinsloo.zander@gmail.com>
2024-01-12 04:25:20 -0500

    data

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Example data set
+#'
+#' @format ## `ex_data`
+#' A data frame with 12651 rows and 4 columns:
+#' \describe{
+#'   \item{status}{Status: either TRUE or FALSE}
+#'   \item{weight}{Survey weight}
+#'   \item{wave}{Wave: either 1, 2, or 3, giving the periiod}
+#'   ...
+#' }
+#' @source self-created
+"ex_data"

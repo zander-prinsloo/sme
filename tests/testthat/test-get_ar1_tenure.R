f9ab00b3f76cf0203fa06270b3bbe24b5ce7ab80
zander-prinsloo <prinsloo.zander@gmail.com>
2024-01-23 08:28:41 -0500

    test ex gaussian function

--- tests/testthat/test-get_ar1_tenure.R
+++ tests/testthat/test-get_ar1_tenure.R
@@ -1,3 +1,21 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+
+
+
+
+test_that("ex_gaussian_density works",{
+
+  expect_equal(
+    ex_gaussian_density(
+    x      = 0,
+    sigma  = 0.1,
+    lambda = 0.5
+  ),
+  gamlss.dist::dexGAUS(
+    x     = 0,
+    mu    = 0,
+    sigma = 0.1,
+    nu    = 0.5
+  )
+  )
+
 })
